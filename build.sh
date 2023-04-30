66017921eeadce668fe058713de9941863453619
Iman Kurniawan <imankurniawan@gmail.com>
2023-04-30 21:39:07 +0700

    catch the date

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-sam build && sam local invoke DownloadFilesFunction 
+# sam build && sam local invoke DownloadFilesFunction 
+sam build && sam local invoke RunTradeFunction 
 # sam sync --stack-name MoneyMachine --watch
 # export SAM_CLI_TELEMETRY=0
 # sam deploy
--- run_trade/app.py
+++ run_trade/app.py
@@ -1,38 +1,86 @@
 import json
+from pyalgotrade import strategy
+from pyalgotrade.barfeed import csvfeed
+from pyalgotrade.bar import Frequency
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import rsi
+from pyalgotrade.technical import cross
+import boto3, tempfile, logging
+import datetime
 
-# import requests
+# Initialize S3 client
+s3 = boto3.client('s3', region_name='ap-southeast-1')
 
+# Set bucket name and file name
+bucket_name = 'imank-pyalgotrade'
+file_name = 'LINKUSDT-1d-all.csv'
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    # Get CSV file from S3
+    obj = s3.get_object(Bucket=bucket_name, Key=file_name)
+    data = obj['Body'].read().decode('utf-8')
+
+    # Write the CSV content to a temporary file
+    temp_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    temp_file.write(data)
+    temp_file.close()
+except Exception as e:
+    logging.info(e)
+class TwoMovingAverageStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, entrySMA, exitSMA, rsiPeriod, overBoughtThreshold):
+        # super(MyStrategy, self).__init__(feed, 1000)
+        super(TwoMovingAverageStrategy, self).__init__(feed, 1000)
+        self.__instrument = instrument
+        self.__rsiPeriod = rsiPeriod
+        self.__entrySMA = ma.SMA(feed[instrument].getCloseDataSeries(), entrySMA)
+        self.__exitSMA = ma.SMA(feed[instrument].getCloseDataSeries(), exitSMA)
+        self.__rsi = rsi.RSI(feed[instrument].getCloseDataSeries(), rsiPeriod)
+        self.__overBoughtThreshold = overBoughtThreshold
+        self.__position = None
+
+    def onEnterOk(self, position):
+        execInfo = position.getEntryOrder().getExecutionInfo()
+        self.info("BUY at %.2f" % (execInfo.getPrice()))
+        if datetime.date.today() == execInfo.getDateTime().date():
+            logger.info('Buy now')
+
+    def onExitOk(self, position):
+        execInfo = position.getExitOrder().getExecutionInfo()
+        self.info("SELL at %.2f" % (execInfo.getPrice()))
+        self.__position = None
+        if datetime.date.today() == execInfo.getDateTime().date():
+            logger.info('Sell now')
+
+    def onBars(self, bars):
+        # Wait for enough bars to be available to calculate SMA
+        if self.__entrySMA[-1] is None or self.__exitSMA[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+
+        # If not already in a position, check for a buy signal
+        if self.__position is None:
+            if cross.cross_above(self.__entrySMA, self.__exitSMA) and self.__rsi[-1] < 30 and self.__rsi[-1] < 50:
+                self.__position = self.enterLong(self.__instrument, 50, True)
+        # If already in a position, check for a sell signal
+        elif cross.cross_below(self.__entrySMA, self.__exitSMA) and self.__rsi[-1] > 70 and self.__rsi[-1] > self.__overBoughtThreshold:
+            self.__position.exitMarket()
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    # Load the bar feed from the CSV file
+    feed = csvfeed.GenericBarFeed(Frequency.DAY) # 5 minutes
+    feed.setDateTimeFormat("%Y-%m-%dT%H:%M:%SZ")
+    feed.addBarsFromCSV("LINKUSDT", temp_file.name)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    # Evaluate the strategy with the feed.
+    # myStrategy = MyStrategy(feed, "LINKUSDT", 20, 4)
+    myStrategy = TwoMovingAverageStrategy(feed, "LINKUSDT", entrySMA=5, exitSMA=4, rsiPeriod=2, overBoughtThreshold=75)
+    myStrategy.run()
+    logger.info("Final portfolio value: $%.2f" % myStrategy.getBroker().getEquity())
     return {
         "statusCode": 200,
         "body": json.dumps({
